ab4a81c7f2905b4418f57eebecae4759b3b512ea
Luke Gehorsam <luke@heroiclabs.com>
2020-08-06 16:06:13 -0400

    add custon Date deserializer to GSON

---
+++ src/main/java/com/heroiclabs/nakama/GsonDateDeserializer.java
@@ -0,0 +1,31 @@
+package com.heroiclabs.nakama;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.Logger;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class GsonDateDeserializer implements JsonDeserializer<Date> {
+
+	@Override
+	public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
+		String date = element.getAsString();
+
+		SimpleDateFormat formatter = new SimpleDateFormat("M/d/yy hh:mm a");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+		try {
+			return formatter.parse(date);
+		}
+		catch (ParseException e) {
+			throw new JsonParseException("Could not deserialze DateTime.");
+		}
+	}
+}
