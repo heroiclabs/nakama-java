caa5abe40869f2d7413db1db4bb5049823a05049
Luke Gehorsam <luke@heroiclabs.com>
2020-08-07 09:53:48 -0400

    add license header and summary to gsondateserializer

--- src/main/java/com/heroiclabs/nakama/GsonDateDeserializer.java
+++ src/main/java/com/heroiclabs/nakama/GsonDateDeserializer.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2020 Heroic Labs
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.heroiclabs.nakama;
 
 import java.lang.reflect.Type;
@@ -5,13 +21,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.logging.Logger;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
+/*
+* A custom Date serializer to indicate to GSON that it is receiving UTC timestamps.
+*/
 public class GsonDateDeserializer implements JsonDeserializer<Date> {
 
 	@Override
