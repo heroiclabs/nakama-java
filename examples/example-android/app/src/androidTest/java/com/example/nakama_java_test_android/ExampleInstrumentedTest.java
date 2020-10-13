9969be59fd2caa72a2fb977a08dcc10c3d9de6e4
Luke Gehorsam <luke@heroiclabs.com>
2020-10-13 13:52:29 -0400

    use more idiomatic android package name for example

--- examples/example-android/app/src/androidTest/java/com/example/nakama_java_test_android/ExampleInstrumentedTest.java
+++ examples/example-android/app/src/androidTest/java/com/example/nakama_java_test_android/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.nakama_java_test_android", appContext.getPackageName());
+        assertEquals("com.example.androidnakama", appContext.getPackageName());
     }
 }
